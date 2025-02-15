452679581033a8f95a12e38048c7607f81b2bc6e
Louifi Mohamed aziz <developer.mohamedazizlouifi@gmail.com>
2025-02-15 06:45:55 +0100

    tam

--- ecommerce/v1/assets/js/main.js
+++ ecommerce/v1/assets/js/main.js
@@ -1134,6 +1134,24 @@
     axilInit.i();
 
 })(window, document, jQuery);
-document.addEventListener("contextmenu", function (event) {
-    event.preventDefault();
+
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+document.addEventListener("keydown", (event) => {
+    if (
+        event.key === "F12" ||
+        (event.ctrlKey && event.shiftKey && event.key === "I") ||
+        (event.ctrlKey && event.shiftKey && event.key === "J") ||
+        (event.ctrlKey && event.key === "U")
+    ) {
+        event.preventDefault();
+    }
 })
+
+setInterval(() => {
+    const devtools = window.outerWidth - window.innerWidth > 160 || window.outerHeight - window.innerHeight > 160;
+    if (devtools) {
+        console.log("Developer tools detected!");
+        window.location.href = "about:blank";
+    }
+}, 1000)
